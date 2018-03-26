72418345426f16f6fe82a57d7edb3dbec607c960
Joao Moita Manrique <joaomoitamanrique@gmail.com>
2018-03-25 22:49:20 -0300

    adicionado os metodos obterSalario() e obterDependentes()

--- grupo6/SalarioFuncionario.java
+++ grupo6/SalarioFuncionario.java
@@ -2,19 +2,47 @@ import java.util.Scanner;
 
 public class SalarioFuncionario {
 
+	private static Scanner scan = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 
-		Scanner read = new Scanner(System.in);
-		
 		System.out.println("Nome do Funcionario: ");
-		String nome = read.next();
+		String nome = scan.next();
 		
-		System.out.println("Salário Bruto:");
-		double salarioB = read.nextDouble();
+		double salarioB = obterSalario();
 		
-		System.out.println("Nº de Dependentes: ");
-		int numDependentes = read.nextInt();
+		int numDependentes = obterDependentes();
 				
+	}
+
+	private static double obterSalario() {
+		System.out.print("Salário Bruto: ");
+		String entrada = scan.nextLine();
+		try{
+			double salario = Double.parseDouble(entrada);
+			if(salario <= 0){
+				throw new Exception();
+			}
+			return salario;
+		} catch (Exception e){
+			System.out.println("Informe um valor real e positivo.");
+			return obterSalario();
+		}
+	}
+
+	private static int obterDependentes() {
+		System.out.print("Nº de Dependentes: ");
+		String entrada = scan.nextLine();
+		try{
+			int NDeDependentes = Integer.parseInt(entrada);
+			if(NDeDependentes < 0){
+				throw new Exception();
+			}
+			return NDeDependentes;
+		} catch (Exception e){
+			System.out.println("Informe um inteiro maior ou igual a zero.");
+			return obterDependentes();
+		}
 	}
 
 }
